d51383e5f385d83bef7d15f9aef798a2b4aed4d9
TEJERINA JOEL CATRIEL <A04598@bh.com.ar>
2023-04-13 12:04:25 -0300

    prueba

--- z-deploy.bat
+++
@@ -1,2 +0,0 @@
-call ng deploy --base-href=/componentes/ --repo=https://gwikidocs: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@github.com/gwikidocs/componentes.git
-pause
--- z-push.bat
+++
@@ -1,2 +0,0 @@
-call git push https://gwikidocs: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@github.com/gwikidocs/componentes.git
-pause
