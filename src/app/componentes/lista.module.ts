27642ad4b7bd66ccb76a0a39cfdad86abcc922aa
TEJERINA JOEL CATRIEL <A04598@bh.com.ar>
2023-04-14 16:58:52 -0300

    componente lista

--- src/app/componentes/lista.module.ts
+++ src/app/componentes/lista.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ListaComponent } from './lista/lista.component';
+import { AvatarModule } from './avatar.module';
+import { AvatarComponent } from './avatar/avatar.component';
 
 
 
 @NgModule({
   declarations: [ListaComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AvatarModule
   ],
-  exports: [ListaComponent]
+  exports: [ListaComponent],
 })
 export class ListaModule { }
--- src/app/componentes/lista/lista.component.ts
+++ src/app/componentes/lista/lista.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lista',
@@ -7,6 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaComponent implements OnInit {
 
+  @Input() icono: string;
+  @Input() textoPrincipal: string;
+  @Input() textoOpcional: string;
+  @Input() textoOpcional2: string;
+  @Input() disabled: string;
+  @Input() to2: string;
+  
   constructor() { }
 
   ngOnInit(): void {
